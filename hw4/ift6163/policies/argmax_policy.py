4b9723c1aacbc90f74e0a139cec9e7f6101ea9df
nappaillav <valliappan0808@gmail.com>
2022-04-01 13:29:53 -0400

    First commit

---
+++ hw4/ift6163/policies/argmax_policy.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class ArgMaxPolicy(object):
+
+    def __init__(self, critic):
+        self.critic = critic
+
+    def get_action(self, obs):
+        if len(obs.shape) > 3:
+            observation = obs
+        else:
+            observation = obs[None]
+        
+        ## TODO return the action that maxinmizes the Q-value 
+        # at the current observation as the output
+        # print(self.critic.qa_values(observation).shape)
+        # print('--------')
+        
+        action = np.argmax(self.critic.qa_values(observation))
+        return action.squeeze()
